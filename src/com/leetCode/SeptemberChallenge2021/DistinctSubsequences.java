6133ff29a4f2ff77849a6452cd09a4c093b3c45c
Dipak Kumar <dipakdsce@gmail.com>
2021-09-22 12:48:44 +0530

    Adding DistinctSubsequences problem of LeetCode

---
+++ src/com/leetCode/SeptemberChallenge2021/DistinctSubsequences.java
@@ -0,0 +1,75 @@
+package com.leetCode.SeptemberChallenge2021;
+
+/*
+Given two strings s and t, return the number of distinct subsequences of s which equals t.
+
+A string's subsequence is a new string formed from the original string by deleting some (can be none) of the characters without disturbing the remaining characters' relative positions. (i.e., "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+
+It is guaranteed the answer fits on a 32-bit signed integer.
+
+
+
+Example 1:
+
+Input: s = "rabbbit", t = "rabbit"
+Output: 3
+Explanation:
+As shown below, there are 3 ways you can generate "rabbit" from S.
+rabbbit
+rabbbit
+rabbbit
+Example 2:
+
+Input: s = "babgbag", t = "bag"
+Output: 5
+Explanation:
+As shown below, there are 5 ways you can generate "bag" from S.
+babgbag
+babgbag
+babgbag
+babgbag
+babgbag
+
+
+Constraints:
+
+1 <= s.length, t.length <= 1000
+s and t consist of English letters.
+ */
+public class DistinctSubsequences {
+    public static int numDistinct(String s, String t) {
+        int m = t.length();
+        int n = s.length();
+
+        if(m>n) {
+            return 0;
+        }
+        int[][] mat = new int[m+1][n+1];
+        for(int i=1; i<=m; i++) {
+            mat[i][0] = 0;
+        }
+
+        for(int j=0; j<=n; j++) {
+            mat[0][j] = 1;
+        }
+
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if(t.charAt(i-1) != s.charAt(j-1)) {
+                    mat[i][j] = mat[i][j-1];
+                } else {
+                    mat[i][j] = mat[i][j-1] + mat[i-1][j-1];
+                }
+            }
+        }
+
+        return mat[m][n];
+    }
+
+    public static void main(String[] args) {
+        String s = "banana";
+        String t = "ban";
+        System.out.println(numDistinct(s, t));
+    }
+
+}
